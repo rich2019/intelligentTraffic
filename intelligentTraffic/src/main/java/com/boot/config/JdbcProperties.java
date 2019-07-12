f2b742ea3a7f094b6ad7ab98a0d9c9b5e0ac25e3
chendongdong <1327571308@qq.com>
2019-07-12 14:13:46 +0800

    直接在DataSource方法上添加注解@ConfigurationProperties(prefix="jdbc"),实现自动装配

--- intelligentTraffic/src/main/java/com/boot/config/JdbcProperties.java
+++ intelligentTraffic/src/main/java/com/boot/config/JdbcProperties.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix = "jdbc")
 @Data
 public class JdbcProperties {
     String driverClassName;
