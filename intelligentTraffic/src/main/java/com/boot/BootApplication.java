c4b6177b67befc4ff61c50c97b92a8a4b3782cb5
chendongdong <1327571308@qq.com>
2019-07-12 10:27:27 +0800

    修改了springboot启动类的位置 启动类必须放在其他类包的外层,否则扫描不到

--- intelligentTraffic/src/main/java/com/boot/BootApplication.java
+++ intelligentTraffic/src/main/java/com/boot/BootApplication.java
@@ -1,7 +1,13 @@
-package myProject;
+package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * 该类必须放在其他包的上层
+ * springboot的bean装配默认规则就是根据application类所在包的位置从上往下扫描
+ */
 
 @SpringBootApplication
 public class BootApplication {
--- intelligentTraffic/src/main/java/com/boot/controller/HelloController.java
+++ intelligentTraffic/src/main/java/com/boot/controller/HelloController.java
@@ -1,14 +1,14 @@
-package controller;
+package com.boot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "hello";
     }
