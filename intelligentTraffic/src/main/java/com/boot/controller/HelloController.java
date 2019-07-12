c3325d07e295cadc12db333ffebe279cf5ca54fd
chendongdong <1327571308@qq.com>
2019-07-12 11:05:19 +0800

    controller中使用了restcontroller注解,不需要responsebody

--- intelligentTraffic/src/main/java/com/boot/controller/HelloController.java
+++ intelligentTraffic/src/main/java/com/boot/controller/HelloController.java
@@ -3,12 +3,12 @@ package com.boot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/hello")
-    @ResponseBody
     public String hello(){
         return "hello";
     }
